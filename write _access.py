cc9cac38ca5ab28dd7f3c0f84d0e874e409e2787
Gerj17 <30609653+Gerj17@users.noreply.github.com>
2021-02-05 15:53:51 -0400

    Add files via upload

---
+++ write _access.py
@@ -0,0 +1,26 @@
+import pyodbc
+from pathlib import Path as p
+
+loc_db = p.cwd().joinpath('Access')  # Create a path to directory with documents
+
+# temp = (str(p.joinpath(loc_db,"Church_database.accdb")))
+try:
+    con_string  = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};'\
+        r'DBQ=C:\\Users\\Ideal\Desktop\\ChurchDataBase-master\\Access\\Church_database.accdb;'
+    conn = pyodbc.connect(con_string)
+    print("connected")
+    # the above path string is the only way it will connect do not change its styokle speciofocally (\\)
+
+    # Add activiteis for the program to enter 
+
+except pyodbc.Error as e:
+    print("there was an error" , e)
+    
+
+def enter_data(con,info):
+    cursor = con.cursor()
+
+    #data in tuple 
+
+    cursor.executemany(f'INSERT INTO {} VALUES (?,?,?)', info) 
+    con.commit()
